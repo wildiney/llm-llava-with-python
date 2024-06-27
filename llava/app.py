1ab63d84eec97c4ab551ccb4d6ca4f6ef0ae1349
Wildiney Di Masi <wildiney@gmail.com>
2024-06-27 14:21:06 -0300

    refactoring

--- llava/app.py
+++ llava/app.py
@@ -1,12 +1,10 @@
 import ollama
 
 res = ollama.chat(
-  model='llava:13b',
+  model='llava:13b', # or llava
   messages=[
     {
     'role':'user',
-    # 'content':'Describe this image',
-    # 'content':'Describe this image C:\\Users\\wildi\\Repositories-Win\\IA-Recognizer\\ia_recognizer\\images\\anya_forger_bond_loid_forger_yor_briar_hd_spy_x_family-2560x1440.jpg',
     'content':'Descreva esta imagem',
     'images':['llava\\images\\image.jpg']
   }]
